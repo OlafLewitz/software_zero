af4ee963082ae2aaa7104ed7c7b90e742083f2a3
Harlan T Wood <harlan@thegoldensun.com>
2012-06-15 18:16:47 -0700

    Remove fedwiki-related code; more permissive with html tags; experiment with returning text only

--- lib/fedwiki/fork.rb
+++ lib/fedwiki/fork.rb
@@ -2,23 +2,21 @@ require 'pismo'
 require 'html_massage'
 require 'rest_client'
 
-require_relative 'random_id'
 require_relative '../env'
 require_relative '../core-ext/nil'
 require_relative '../../config/initializers/string'
 
 module FedWiki
 
-  class NoKnownOpenLicense < RuntimeError ; end
+  class NoKnownOpenLicense < RuntimeError;
+  end
 
-  SUBDOMAIN_PATTERN = "[a-zA-Z0-9][a-zA-Z0-9-]{0,62}"  # Subdomain 'segments' are 1 - 63 characters.  Although technically lower case, URLs may come in as mixed case.
+  SUBDOMAIN_PATTERN = "[a-zA-Z0-9][a-zA-Z0-9-]{0,62}" # Subdomain 'segments' are 1 - 63 characters.  Although technically lower case, URLs may come in as mixed case.
 
   OPEN_LICENSE_PATTERNS = %w[
     gnu.org/licenses
     creativecommons.org/licenses
   ]
-
-  SFW_BASE_DOMAIN = Env['SFW_BASE_DOMAIN'] || raise("please set the environment variable SFW_BASE_DOMAIN")
 
   class << self
     def open_site(data, options={})
@@ -48,12 +46,12 @@ module FedWiki
 
       html = doc.to_s
 
-      metadata = Pismo::Document.new(html) rescue nil  # pismo occasionally crashes, eg on invalid UTF8
-                 # for a list of metadata properties, see https://github.com/peterc/pismo
-                 # To limit keywords to specific items we care about, consider this doc fragment --
-                 #   New! The keywords method accepts optional arguments. These are the current defaults:
-                 #   :stem_at => 20, :word_length_limit => 15, :limit => 20, :remove_stopwords => true, :minimum_score => 2
-                 #   You can also pass an array to keywords with :hints => arr if you want only words of your choosing to be found.
+      metadata = Pismo::Document.new(html) rescue nil # pismo occasionally crashes, eg on invalid UTF8
+                                                      # for a list of metadata properties, see https://github.com/peterc/pismo
+                                                      # To limit keywords to specific items we care about, consider this doc fragment --
+                                                      #   New! The keywords method accepts optional arguments. These are the current defaults:
+                                                      #   :stem_at => 20, :word_length_limit => 15, :limit => 20, :remove_stopwords => true, :minimum_score => 2
+                                                      #   You can also pass an array to keywords with :hints => arr if you want only words of your choosing to be found.
 
       return if html.empty? || !metadata || url =~ /%23/ # whats up with %23?
 
@@ -78,27 +76,20 @@ module FedWiki
       connector = options[:domain_connector]
       curator = options[:username] || Env['CURATOR']
 
-      subdomain = [subject, connector, curator, connector].compact.map{|segment| segment.slug}.join('.')
-      sfw_site = "#{subdomain}.#{Env['SFW_BASE_DOMAIN']}"
-      sfw_action_url = "http://#{sfw_site}/page/#{slug}/action"
+      subdomain = [subject, connector, curator, connector].compact.map { |segment| segment.slug }.join('.')
 
       #############
 
       title = extract_title(doc) || metadata.title
       keywords = metadata.keywords.map(&:first)
 
-      sfw_page_data = {
-        'title' => title,
-        'keywords' => keywords,
-        'license_links' => license_links,
-        'story' => [],
-      }
-
-      # Two ways to check the last updated time, both unsatisfactory...
-      # sfw_page_data.merge! 'updated_at' => page.headers['Last-Modified']
-      # sfw_page_data.merge! 'updated_at' => meta.datetime.utc.iso8601 if meta.datetime rescue nil
-
-      #ap sfw_page_data
+      # TODO: feed this data into page metadata -- markdown yaml front matter and/or html meta tags?
+      #sfw_page_data = {
+      #  'title' => title,
+      #  'keywords' => keywords,
+      #  'license_links' => license_links,
+      #  'story' => [],
+      #}
 
       #############
 
@@ -106,53 +97,49 @@ module FedWiki
       html = remove_first_h1_if_same_as_title(html, title)
       html = convert_links_to_crawled_pages_to_wikilinks(html, origin_domain, options[:site_urls])
       html.strip_lines!
-      html_chunks = html.split(/\n{2,}/)
+      html.gsub!(/\n{3,}/, "\n\n")
+      #html_chunks = html.split(/\n{2,}/)
       sep = [%{<hr />}]
       attribution_html = [%{This page was forked with permission from <a href="#{url}" target="_blank">#{url}</a>}]
 
-      (html_chunks + sep + attribution_html + sep + license_links).each do |html_chunk|
-        sfw_page_data['story'] << ({
-          'type' => 'paragraph',
-          'id' => RandomId.generate,
-          'text' => html_chunk
-        })
-      end
+      html += (sep + attribution_html + sep + license_links).join("\n\n")
 
       #############
 
-      push_to_github :path => "#{slug}.json", :content => JSON.pretty_generate(sfw_page_data), :repo => 'test'
+      push_to_github :path => "#{slug}.html", :content => html, :repo => 'test'
 
       #############
 
-      begin
-        sfw_do(sfw_action_url, :create, sfw_page_data)
-      rescue RestClient::Conflict
-        sfw_do(sfw_action_url, :update, sfw_page_data)
-      end
-
-      fork_url = "http://#{sfw_site}/view/#{slug}"
-      fork_url
+      [ subdomain, slug ]
     end
 
     def massage_html(html, url)
       sanitize_options = HtmlMassage::DEFAULT_SANITIZE_OPTIONS.merge(
-          :elements => %w[
+        :elements => %w[
             a img
-            h1 h2 h3 hr
-            table th tr td
-            em strong b i
+            hr p
+            h1 h2 h3 h4 h5 h6
+
+            table tbody th tr td
+            ul ol li
+            dd dl dt
+
+            b i em strong
+            small strike
+            sub sup
+
+            blockquote
+            code pre
           ],
-          :attributes => {
-            :all => [],
-            'a' => %w[ href ],
-            'img' => %w[ src alt ],
-          }
+        :attributes => {
+          :all => [],
+          'a' => %w[ href ],
+          'img' => %w[ src alt ],
+          'td' => %w[ colspan rowspan ],
+        }
       )
-      begin
-        HtmlMassage.html html, :source_url => url, :links => :absolute, :images => :absolute, :sanitize => sanitize_options
-      rescue Encoding::CompatibilityError
-        return # TODO: manage this inside the html_massage gem!
-      end
+      #HtmlMassage.html html, :source_url => url, :links => :absolute, :images => :absolute, :sanitize => sanitize_options
+      HtmlMassage.text html, :source_url => url, :links => :absolute, :images => :absolute, :sanitize => sanitize_options
     end
 
     def open_license_links(doc)
@@ -200,20 +187,10 @@ module FedWiki
           if site_urls.include?(match[0])
             link_slug = match['href_path'].slug
             link['href'] = link_slug
-            link['class'] = "#{link['class']} fedwiki-internal".strip # the class is for later client-side processing
           end
         end
       end
       doc.to_html
-    end
-
-    def sfw_do(sfw_action_url, action, sfw_page_data)
-      action_json = JSON.pretty_generate 'type' => action, 'item' => sfw_page_data
-      #begin
-      RestClient.put "#{sfw_action_url}", :action => action_json, :content_type => :json, :accept => :json
-      #rescue RestClient::ResourceNotFound
-      #  puts "!!! ERROR: SFW SERVER NOT FOUND at #{sfw_action_url}"
-      #end
     end
 
     def push_to_github(params)
