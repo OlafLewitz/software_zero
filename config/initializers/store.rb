1e433d2a96e9741f4c265bfec406e36e27120ae0
Harlan T Wood <harlan@thegoldensun.com>
2012-06-19 22:46:29 -0700

    Reload github_store when changed

--- config/initializers/store.rb
+++ config/initializers/store.rb
@@ -1,4 +1,5 @@
-require_dependency File.expand_path("../../lib/stores/all", File.dirname(__FILE__))
+require            File.expand_path("../../lib/stores/all",          File.dirname(__FILE__))
+require_dependency File.expand_path("../../lib/stores/github_store", File.dirname(__FILE__))
 
 Store.set 'GithubStore', Rails.root
 
