36e459f00a70c7450f25fd4ef2910b21d5ef4c3e
Harlan T Wood <harlan@thegoldensun.com>
2012-06-29 02:24:47 -0700

    Rotate random banner images, each session

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def banner_image
+    cookies[:banner_image] ||= random_banner_image
+  end
+
+  # eg: "/assets/skins/appstoreforyourhead/headers/2.png"
+  def random_banner_image
+    headers_paths = Dir.chdir(Rails.root.join('app/assets/images')) { Dir["skins/appstoreforyourhead/headers/*.png"] }
+    headers_paths.map!{ |path| File.join('/assets', path) }
+    headers_paths.sample
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-OpenYourProject::Application.config.session_store :cookie_store, key: '_open_your_project_session'
+OpenYourProject::Application.config.session_store :cookie_store,
+                                                  key: '_open_your_project_session',
+                                                  :expire_after => 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
