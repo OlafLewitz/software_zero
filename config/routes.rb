b5dd646793216252d20b1f6770df6aeedc52cb4f
Harlan T Wood <harlan@thegoldensun.com>
2012-06-28 22:53:19 -0700

    Same home page always

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require File.expand_path("../lib/env", File.dirname(__FILE__))
 
 OpenYourProject::Application.routes.draw do
 
-  root :to => Env['CREATE_PAGES'] ? 'pages#new' : 'collections#index'
+  root :to => 'collections#index'
 
   resources :collections, :only => %w[ index ]
   resources :pages, :only => %w[ new create edit update index ] if Env['CREATE_PAGES']
