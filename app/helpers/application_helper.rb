31861778d44a45f5b1f4cc93a6602c7654de1880
Harlan T Wood <harlan@thegoldensun.com>
2012-07-01 00:33:23 -0700

    Only look for banner images if env var SKIN is set

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
+
+  def banner_style
+    "background-image: url(#{banner_image});" if Env['SKIN']
+  end
+
   def banner_image
     cookies[:banner_image] ||= random_banner_image
   end
 
-  # eg: "/assets/skins/appstoreforyourhead/headers/2.png"
+  # returns something like: "/assets/skins/appstoreforyourhead/headers/2.png"
   def random_banner_image
-    headers_paths = Dir.chdir(Rails.root.join('app/assets/images')) { Dir["skins/appstoreforyourhead/headers/*.png"] }
+    headers_paths = Dir.chdir(Rails.root.join('app/assets/images')) { Dir["skins/#{Env['SKIN']}/headers/*.png"] }
     headers_paths.map!{ |path| File.join('/assets', path) }
     headers_paths.sample
   end
+
 end
