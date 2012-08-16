76a596462eb5334d8de0abaa40e6e868ee0dd614
Harlan T Wood <harlan@thegoldensun.com>
2012-08-15 23:57:30 -0700

    Handle git/github failures more gracefully

--- lib/stores/github_store.rb
+++ lib/stores/github_store.rb
@@ -1,6 +1,7 @@
 require 'github_api'
 require_relative 'store'
 require_relative '../run_cmd'
+require_relative '../core-ext/nil'
 
 class GithubStore < Store
   class << self
@@ -10,10 +11,10 @@ class GithubStore < Store
     ### GET
 
     def get_text(path, metadata)
-      repo = metadata[:repo] || metadata[:subdomain] || raise('Please pass in metadata[:repo] or metadata[:subdomain]')
-      raw_url = "https://raw.github.com/#{Env['GITHUB_USER']}/#{repo}/master/#{path}"
+      repo = repo_for_reading(metadata)
       begin
-        RestClient.get raw_url
+        puts "Getting content from #{raw_url(repo, path)} ..."
+        RestClient.get raw_url(repo, path)
       rescue RestClient::ResourceNotFound
         nil
       end
@@ -28,17 +29,20 @@ class GithubStore < Store
       raise(ArgumentError, "Expected argument 'text'") unless text
       user = ENV['GITHUB_USER'] || raise("Please set env var GITHUB_USER")
       password = ENV['GITHUB_PASS'] || raise("Please set env var GITHUB_PASS")
-      repo = metadata[:subdomain]
+      repo_name = repo_for_writing(metadata)
+      puts "Putting content to:"
+      puts "https://github.com/#{ENV['GITHUB_USER']}/#{repo_name}/blob/master/#{path}"
 
       @github = Github.new :basic_auth => "#{user}:#{password}"
 
-      repo! user, repo
-      last_commit_sha = @github.git_data.references.get(user, repo, 'heads/master').object.sha
-      last_tree_sha = @github.git_data.commits.get(user, repo, last_commit_sha).tree.sha
+      repo!(user, repo_name)
+      last_commit_sha = reference!(user, repo_name, 'heads/master').object.sha
+      last_tree_sha = @github.git_data.commits.get(user, repo_name, last_commit_sha).tree.sha
 
 
       # create tree object (also implicitly creates a blob based on content)
-      new_content_tree_sha = @github.git_data.trees.create(user, repo,
+      begin
+        new_content_tree_sha = @github.git_data.trees.create(user, repo_name,
                                                        :base_tree => last_tree_sha,
                                                        :tree => [
                                                          {
@@ -47,15 +51,27 @@ class GithubStore < Store
                                                            :mode => '100644'
                                                          }
                                                        ]).sha
+      rescue Faraday::Error::TimeoutError => exception
+        STDERR.puts "******************"
+        STDERR.puts "We got a Faraday::Error::TimeoutError"
+        STDERR.puts "user:          #{user}"
+        STDERR.puts "repo:          #{repo_name}"
+        STDERR.puts "last_tree_sha: #{last_tree_sha}"
+        STDERR.puts "path:          #{path}"
+        STDERR.puts "text:"
+        STDERR.puts text
+        STDERR.puts "******************"
+        raise exception
+      end
 
-      new_commit_sha = @github.git_data.commits.create(user, repo,
+      new_commit_sha = @github.git_data.commits.create(user, repo_name,
                                                        :parents => [last_commit_sha],
                                                        :tree => new_content_tree_sha,
                                                        :message => "Update #{path}"
       ).sha
 
       # update branch to point to new commit
-      @github.git_data.references.update(user, repo, 'heads/master', :sha => new_commit_sha)
+      @github.git_data.references.update(user, repo_name, 'heads/master', :sha => new_commit_sha)
 
       text
     end
@@ -77,11 +93,67 @@ class GithubStore < Store
         # So we create one manually here via the command line, and push it.
         # Requires git to be in your path.
         remote = "https://#{ENV['GITHUB_USER']}:#{ENV['GITHUB_PASS']}@github.com/#{ENV['GITHUB_USER']}/#{repo_name}.git"
-        run "mkdir -p #{File.join(@app_root, 'zero')}"
-        run "cd #{File.join(@app_root, 'zero')} && git clone #{repo.clone_url}"
-        run "cd #{File.join(@app_root, 'zero', repo_name)} && git commit --allow-empty -m 'Initial commit'"
-        run "cd #{File.join(@app_root, 'zero', repo_name)} && git push --quiet #{remote} HEAD"
+
+        local_repos_dir = File.join @app_root, 'zero'
+        local_repo_dir = File.join local_repos_dir, repo_name
+        author_name = ENV['GITHUB_USER']
+        author_email = "#{ENV['GITHUB_USER']}@(none)"
+        author_env_vars = "GIT_COMMITER_NAME='#{author_name}' GIT_COMMITTER_EMAIL='#{author_email}'"
+        author = "#{author_name} <#{author_email}>"
+
+        run "mkdir -p #{local_repos_dir}"
+        run "rm -rf #{local_repo_dir}"
+        run "cd #{local_repos_dir} && git clone https://github.com/#{ENV['GITHUB_USER']}/#{repo_name}.git"
+        run "cd #{local_repo_dir} && #{author_env_vars} git commit --allow-empty --author='#{author}' -m 'Initial commit'"
+        run "cd #{local_repo_dir} && git push --quiet #{remote} HEAD"
+        repo
       end
+    end
+
+    def reference!(user, repo_name, ref_name)
+      begin
+        @github.git_data.references.get(user, repo_name, ref_name)
+      rescue Github::Error::ServiceError
+        remote = "https://#{ENV['GITHUB_USER']}:#{ENV['GITHUB_PASS']}@github.com/#{ENV['GITHUB_USER']}/#{repo_name}.git"
+        local_repos_dir = File.join @app_root, 'zero'
+        local_repo_dir = File.join local_repos_dir, repo_name
+
+        run "mkdir -p #{local_repos_dir}"
+        run "rm -rf #{local_repo_dir}"
+        run "cd #{local_repos_dir} && git clone https://github.com/#{ENV['GITHUB_USER']}/#{repo_name}.git"
+        run "cd #{local_repo_dir} && git commit --allow-empty -m 'Initial commit'"
+        run "cd #{local_repo_dir} && git push --quiet #{remote} HEAD"
+
+        @github.git_data.references.get(user, repo_name, ref_name)
+      end
+    end
+
+    ### UTIL
+
+    def raw_url(repo, path)
+      "https://raw.github.com/#{Env['GITHUB_USER']}/#{repo}/master/#{path}"
+    end
+
+    private
+
+    def repo_for_reading(metadata)
+      extract_repo(metadata, :subdomain)
+    end
+
+    def repo_for_writing(metadata)
+      extract_repo(metadata, :padded_subdomain)
+    end
+
+    def extract_repo(metadata, slug_format=nil)
+      keys = [:repo, :collection]
+      keys.each{ |key| return metadata[key] unless metadata[key].empty? }
+      raise("Please pass in #{keys.map{|key| "metadata[:#{key}]"}.join(' or ')}")
+
+      #keys = [:repo, :collection]
+      #keys.each{ |key| return metadata[key].slug(slug_format) unless metadata[key].empty? }
+      #keys = [:subdomain]
+      #keys.each{ |key| return metadata[key] unless metadata[key].empty? }
+      #raise("Please pass in #{keys.map{|key| "metadata[:#{key}]"}.join(' or ')}")
     end
 
   end
